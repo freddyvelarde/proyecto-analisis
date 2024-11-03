0acc9b340114416354697c057f678d06b91a7473
freddyvelarde <freddvel.dev@gmail.com>
2024-11-03 10:50:00 -0400

    onclick event debugging again

---
+++ script/scrollToTop.js
@@ -0,0 +1,7 @@
+function scrollToTop() {
+  console.log("helloworld from scroll to top");
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
--- script/toggleSidebar.js
+++ script/toggleSidebar.js
@@ -1,12 +1,5 @@
 function toggleSidebar() {
-  console.log("toggle to top function triggered"); // For debugging
+  console.log("thello world from toggle ");
   const sidebar = document.querySelector(".sidebar");
   sidebar.style.display = sidebar.style.display === "none" ? "block" : "none";
-}
-function scrollToTop() {
-  console.log("Scroll to top function triggered"); // For debugging
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth",
-  });
 }
