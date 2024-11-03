1d268391055ebc289c0fb25f9550765a46ca901e
freddyvelarde <freddvel.dev@gmail.com>
2024-11-03 10:45:46 -0400

    onclick event debugging

--- script/toggleSidebar.js
+++ script/toggleSidebar.js
@@ -1,8 +1,10 @@
 function toggleSidebar() {
+  console.log("toggle to top function triggered"); // For debugging
   const sidebar = document.querySelector(".sidebar");
   sidebar.style.display = sidebar.style.display === "none" ? "block" : "none";
 }
 function scrollToTop() {
+  console.log("Scroll to top function triggered"); // For debugging
   window.scrollTo({
     top: 0,
     behavior: "smooth",
