affe4262b507f1cf061c05915e8d90473888950c
freddyvelarde <freddvel.dev@gmail.com>
2024-11-03 10:41:46 -0400

    some changes

--- script/toggleSidebar.js
+++ script/toggleSidebar.js
@@ -2,3 +2,9 @@ function toggleSidebar() {
   const sidebar = document.querySelector(".sidebar");
   sidebar.style.display = sidebar.style.display === "none" ? "block" : "none";
 }
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
